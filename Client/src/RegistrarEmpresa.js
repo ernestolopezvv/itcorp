2230d9e22a960add93af75df87a63f0d73cda2ef
andykandujo <a00227491@itesm.mx>
2022-04-22 12:30:30 -0600

    Update RegistrarEmpresa

--- Client/src/RegistrarEmpresa.js
+++ Client/src/RegistrarEmpresa.js
@@ -9,8 +9,16 @@ function RegistrarEmpresa () {
         <div className="main">
           <div className = "titulo"><h1>Agregar Nueva Empresa</h1></div>
             <div className = "centeredContainer">
-              <button onClick={()=> history("/MenuAdmin")}>Regresar a Menú Admin</button>
-              
+              <button onClick={()=> history("/MenuAdmin")}>Regresar a Menú Admin</button>        
+            </div>
+            <div className= "inputInformation">
+                <label>Nombre de la empresa: </label> 
+                <input type="text" />  
+                <label>Tipo de moneda:</label> 
+                <input type="text" />   
+            </div> 
+            <div className = "centeredContainer">
+              <button>Agregar nueva empresa</button>        
             </div>
           </div>
     );

06c71b25ebfbc1a2abb3b95db874c828b3a745bd
José Enrique Ruiz Acosta <jera_24@outlook.com>
2022-04-25 10:58:22 -0600

    Post archivo

--- Client/src/SubirReporte.js
+++ Client/src/SubirReporte.js
@@ -1,16 +1,19 @@
 import Axios  from "axios";
 import React, { useState } from "react";
+// import FormData from "form-data";
 import {useNavigate} from "react-router-dom";
+
 
 function SubirReporte () {
 
   const [archivo, setArchivo] = useState("");
 
-  const subirArchivo = () => {
-    console.log(archivo);
-    Axios.post("http://localhost:3001/api/xlsx", {
-  })
-};
+
+//   const subirArchivo = () => {
+//     console.log(archivo);
+//     Axios.post("http://localhost:3001/api/xlsx", {
+//   })
+// };
 
   // handleFile(e) 
   // {
@@ -20,26 +23,25 @@ function SubirReporte () {
  
     // a local state to store the currently selected file.
     const [selectedFile, setSelectedFile] = React.useState(null);
+
   
     const handleSubmit = (event) => {
       event.preventDefault()
-      const formData = new FormData();
-      formData.append("selectedFile", selectedFile);
+
+      const form = new FormData();
+      form.append("file", selectedFile);
+      
       console.log(selectedFile);
-      console.log(selectedFile.data);
-      console.log(formData);
       try {
-        const response = Axios({
-          method: "post",
-          url: "http://localhost:3001/api/xlsx",
-          data: selectedFile,
-          headers: { "Content-Type": "multipart/form-data" },
-          // headers: { "Nivel": "", "  C ó d i g o": "", "N o m b r e": "", "T i p o": "" },
+        const response = Axios.post( "http://localhost:3001/api/xlsx", form, {
+      //     // data: selectedFile,
+      //     headers: { "Content-Type": "multipart/form-data" },
+      //     // headers: { "Nivel": "", "  C ó d i g o": "", "N o m b r e": "", "T i p o": "" },
 
         });
       } catch(error) {
         console.log(error)
-      }
+      };
     }
   
     const handleFileSelect = (event) => {
@@ -50,26 +52,21 @@ function SubirReporte () {
 
 
     return(
-        // <div className="main">
-        //   <div className = "titulo"><h1>Subir Reporte</h1></div>
-        //     <div className = "centeredContainer">
-        //       <button onClick={()=> history("/SeleccionarEmpresa")}>Regresar a Seleccionar empresa</button>
-        //       <input type="file" name="file" onChange={(event)=> {setArchivo(event.target.value)}} />
-        //       <script> 
-        //         function log_console() {
-        //           console.log(archivo)
-        //         }
-        //       </script>
-        //       {/* <input type="file" name="file" onChange={(e)=>this.onChange(e)} /> */}
-        //       {/* <input type="file" name="file" onChange={(e)=>this.handleFile(e)} /> */}
-        //       <button className="button" onClick={subirArchivo}>Subir Reporte</button>
-        //     </div>
-        //   </div>
+        <div className="main">
+          <div className = "titulo"><h1>Subir Reporte</h1></div>
+            <div className = "centeredContainer">
+              <button onClick={()=> history("/SeleccionarEmpresa")}>Regresar a Seleccionar empresa</button>
+              <br/>
+              <input type="file" onChange={handleFileSelect}/>
 
-        <form onSubmit={handleSubmit}>
-        <input type="file" onChange={handleFileSelect}/>
-        <input type="submit" value="Upload File" />
-      </form>
+              
+              <form onSubmit={handleSubmit}> <br/>
+              <input type="submit" value="Subir Reporte" />
+              </form>
+
+            </div>
+          </div>
+
     );
 } 
 

ce4d5b2691cc22f68d623a985ee4318350ab42b7
ErickSolisMahl <A00904052@itesm.mx>
2022-06-16 17:35:56 -0700

    Update EstadoResultados.js

--- src/components/reports/EstadoResultados.js
+++ src/components/reports/EstadoResultados.js
@@ -1,28 +1,26 @@
-import {useNavigate} from "react-router-dom";
-import Select from 'react-select';
+import { useNavigate } from "react-router-dom";
+import Select from "react-select";
 import { useRef, useState, useEffect } from "react";
-import axios from '../../api/axios';
-import { AgGridReact } from 'ag-grid-react';
+import axios from "../../api/axios";
+import { AgGridReact } from "ag-grid-react";
 import useAuth from "../../hooks/useAuth";
 
-const USER_COMPANIES_URL = 'companies/accessCompanies';
-const ESTADO_RESULTDOS_URL = 'EstadoResultados';
-
+const USER_COMPANIES_URL = "companies/accessCompanies";
+const ESTADO_RESULTDOS_URL = "EstadoResultados";
 
 const EstadoResultados = () => {
-
   const history = useNavigate();
-  
-  const[listaEgresos, setListaEgresos] = useState([]);
+
+  const [listaEgresos, setListaEgresos] = useState([]);
 
   //const [gridApi, setGridApi] = useState()
-  const [fechaInicio, setFechaInicio] = useState('');
-  const [fechaFinal, setFechaFinal] = useState('');
-  const sectionName = [' '];
-  const [fechaInicioComp, setFechaInicioComp] = useState('');
-  const [fechaFinalComp, setFechaFinalComp] = useState('');
+  const [fechaInicio, setFechaInicio] = useState("");
+  const [fechaFinal, setFechaFinal] = useState("");
+  const sectionName = [" "];
+  const [fechaInicioComp, setFechaInicioComp] = useState("");
+  const [fechaFinalComp, setFechaFinalComp] = useState("");
 
-  const colNames = [' ','Periodo', '%', 'Acomulado', '%'];
+  const colNames = [" ", "Periodo", "%", "Acomulado", "%"];
 
   // Select Company
   const [companies, setCompanies] = useState([]);
@@ -33,36 +31,31 @@ const EstadoResultados = () => {
   const userInfo = userLoginInfo.auth;
   const idEmpresa = selectedOption.ID_Empresa;
 
-  
   const getData = async (e) => {
+    const fechaI = fechaInicio;
+    const fechaF = fechaFinal;
 
-      const fechaI = fechaInicio
-      const fechaF = fechaFinal
-      
-      try{
-
-          const response = await axios.post(ESTADO_RESULTDOS_URL, {fechaI, fechaF, idEmpresa});
-            console.log(fechaI);
-            console.log(fechaF);
-            return setListaEgresos(response.data);
-        }
-
-         catch (err) {
-          console.log(err);
-
-      }
-  }
+    try {
+      const response = await axios.post(ESTADO_RESULTDOS_URL, {
+        fechaI,
+        fechaF,
+        idEmpresa,
+      });
+      console.log(fechaI);
+      console.log(fechaF);
+      return setListaEgresos(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
     const fetchCompanies = async () => {
       try {
-
         const response = await axios.post(USER_COMPANIES_URL, userInfo);
         setCompanies(response.data);
-
       } catch (err) {
         if (err.response) {
-
           console.log(err.response.data);
           console.log(err.response.status);
           console.log(err.response.headers);
@@ -70,96 +63,126 @@ const EstadoResultados = () => {
           console.log(`Error: ${err.message}`);
         }
       }
-
-    }
+    };
     fetchCompanies();
+  }, []);
 
-  }, [])
-
-  const handleOnChange = Obj => {
+  const handleOnChange = (Obj) => {
     setSelectedOption(Obj);
+  };
 
-  }
-
-  const handleOnChangeFechaInicio = Obj => {
+  const handleOnChangeFechaInicio = (Obj) => {
     setFechaInicio(Obj.target.value);
     setFechaInicioComp(true);
-  }
+  };
 
-  const handleOnChangeFechaFinal = Obj => {
+  const handleOnChangeFechaFinal = (Obj) => {
     setFechaFinal(Obj.target.value);
     setFechaFinalComp(true);
-  }
+  };
 
+  return (
+    <div className="menubox">
+      <div className="sub-menubox2">
+        <div>
+          <h1>Ver Estado de Resultados</h1>
 
-
-  return(
-
-    <div className="main">
-          <div className = "titulo"><h1>Ver Estado de Resultados</h1></div>
-          <div className = "centeredContainer">
-                <button onClick={()=> history("/reportes")}>Regresar a Menú Reporte</button>  
-                <button onClick={createPDF} >Descargar en PDF</button>      
+          <div className="menu-button">
+            <button className="button3" onClick={() => history("/reportes")}>
+              Regresar a Menú Reporte
+            </button>
           </div>
 
+          <div className="menu-button">
+            <Select
+              defaultInputValue={selectedOption}
+              onChange={handleOnChange}
+              options={companies}
+              getOptionLabel={(option) => option.Nombre}
+            />
+          </div>
 
+          <div className="menu-button">
+            From :{" "}
+            <input
+              type="date"
+              value={fechaInicio}
+              onChange={handleOnChangeFechaInicio}
+            />
+            To :{" "}
+            <input
+              type="date"
+              value={fechaFinal}
+              onChange={handleOnChangeFechaFinal}
+            />
+          </div>
 
-          <Select defaultInputValue={selectedOption}
-            onChange={handleOnChange}
-            options={companies}
-            getOptionLabel={option => option.Nombre} />
-        
+          <div className="menu-button">
+            <button
+              className="button3"
+              onClick={getData}
+              disabled={
+                selectedOption === ""
+                  ? true
+                  : false || fechaInicioComp !== true || fechaFinalComp !== true
+              }
+            >
+              {" "}
+              Generar Estado de Resultados{" "}
+            </button>
+          </div>
 
+          <div className="menu-button">
+            <button className="button3" onClick={createPDF}>
+              Descargar en PDF
+            </button>
+          </div>
+        </div>
+      </div>
 
-          <div className="ag-theme-alpine" style={{ height: 400 }}>
-            From : <input type="date" value={fechaInicio} onChange={handleOnChangeFechaInicio} />
-            To : <input type="date" value={fechaFinal} onChange={handleOnChangeFechaFinal} />
-            </div>
-            
-          <div className="ReporteResultados">
-
-          <button onClick={getData} 
-              disabled={selectedOption === '' ? true : false
-              || fechaInicioComp !== true
-              || fechaFinalComp !== true}> Generar Estado de Resultados </button>
-
-          <div className="Table" id="Table">
+      <div className="ReporteResultados">
+        <div className="Table" id="Table">
           {listaEgresos.length > 0 && (
-          <table className="table ">
-                <thead>
+            <table className="table ">
+              <thead>
                 <tr>
-                      {colNames.map((headerItem, uno) => (
-                            <th key={uno}>
-                            {headerItem.toUpperCase()}
-                                </th>
-                            ))}
+                  {colNames.map((headerItem, uno) => (
+                    <th key={uno}>{headerItem.toUpperCase()}</th>
+                  ))}
                 </tr>
-                </thead>
-                
-                <tbody>
+              </thead>
+
+              <tbody>
                 <td>Ingresos </td>
                 <tr>
                   <td>{listaEgresos[0].Nombre1}</td>
-                  <td>{listaEgresos[0].Abono_Total1} {listaEgresos[0].Abono_Total1 ? '' : '0'}</td>
+                  <td>
+                    {listaEgresos[0].Abono_Total1}{" "}
+                    {listaEgresos[0].Abono_Total1 ? "" : "0"}
+                  </td>
                   <td>{listaEgresos[0].PorcentajePi}</td>
-                  <td>{listaEgresos[0].Saldo_Total1} {listaEgresos[0].Saldo_Total1 ? '' : '0'}</td>
+                  <td>
+                    {listaEgresos[0].Saldo_Total1}{" "}
+                    {listaEgresos[0].Saldo_Total1 ? "" : "0"}
+                  </td>
                   <td>{listaEgresos[0].PorcentajeAi}</td>
                 </tr>
-                </tbody>
+              </tbody>
 
-                <tbody>
+              <tbody>
                 <td>Egresos </td>
-                {Object.values(listaEgresos).map((val, tres) =>(
-                      <tr key={tres}>
-                            <td> {val.Nombre} </td>
-                            <td> {val.Cargo_Cuenta} </td>
-                            <td> {val.PorcentajeP} </td>
-                            <td> {val.Saldo_Cuenta} </td>
-                            <td> {val.PorcentajeA} </td>
-                      </tr>
-                 ))} </tbody>
+                {Object.values(listaEgresos).map((val, tres) => (
+                  <tr key={tres}>
+                    <td> {val.Nombre} </td>
+                    <td> {val.Cargo_Cuenta} </td>
+                    <td> {val.PorcentajeP} </td>
+                    <td> {val.Saldo_Cuenta} </td>
+                    <td> {val.PorcentajeA} </td>
+                  </tr>
+                ))}{" "}
+              </tbody>
 
-                <tbody>
+              <tbody>
                 <td>Total Egresos </td>
                 <tr>
                   <td></td>
@@ -168,55 +191,49 @@ const EstadoResultados = () => {
                   <td>{listaEgresos[0].Saldo_Totali}</td>
                   <td></td>
                 </tr>
-                 </tbody>
+              </tbody>
 
-                <tbody>
+              <tbody>
                 <td>Utilidad </td>
                 <tr>
                   <td></td>
-                  <td>{listaEgresos[listaEgresos.length-1][0]}</td>
+                  <td>{listaEgresos[listaEgresos.length - 1][0]}</td>
                   <td></td>
-                  <td>{listaEgresos[listaEgresos.length-1][1]}</td>
+                  <td>{listaEgresos[listaEgresos.length - 1][1]}</td>
                   <td></td>
                 </tr>
-                </tbody>
-                
-          </table>
-          
+              </tbody>
+            </table>
           )}
-          
-            </div>
-          </div>
-          
+        </div>
+      </div>
     </div>
+  );
 
-)
+  function createPDF() {
+    var sTable = document.getElementById("Table").innerHTML;
 
-function createPDF() {
-  var sTable = document.getElementById('Table').innerHTML;
+    var style = "<style>";
+    style = style + "table {width: 100%;font: 10px Calibri;}";
+    style =
+      style +
+      "table, th, td {border: solid 2px #DDD; border-collapse: collapse;";
+    style = style + "padding: 2px 3px;text-align: center;}";
+    style = style + ".left{text-align:left};.right{text-align:right};";
+    style = style + "</style>";
 
-  var style = "<style>";
-  style = style + "table {width: 100%;font: 10px Calibri;}";
-  style = style + "table, th, td {border: solid 2px #DDD; border-collapse: collapse;";
-  style = style + "padding: 2px 3px;text-align: center;}";
-  style = style + ".left{text-align:left};.right{text-align:right};"
-  style = style + "</style>";
+    // CREATE A WINDOW OBJECT.
+    var win = window.open("", "", "height=700,width=700");
 
-  // CREATE A WINDOW OBJECT.
-  var win = window.open('', '', 'height=700,width=700');
+    win.document.write(style); // ADD STYLE INSIDE THE HEAD TAG.
+    win.document.write("</head>");
+    win.document.write(sTable); // THE TABLE CONTENTS INSIDE THE BODY TAG.
+    win.document.write("</body></html>");
 
-  win.document.write(style);          // ADD STYLE INSIDE THE HEAD TAG.
-  win.document.write('</head>');
-  win.document.write(sTable);         // THE TABLE CONTENTS INSIDE THE BODY TAG.
-  win.document.write('</body></html>');
+    win.document.close(); // CLOSE THE CURRENT WINDOW.
 
-  win.document.close(); 	// CLOSE THE CURRENT WINDOW.
-
-  win.print();    // PRINT THE CONTENTS.
-}
-
-
-}
-
+    win.print(); // PRINT THE CONTENTS.
+  }
+};
 
 export default EstadoResultados;
